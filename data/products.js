561ff95cf5e643f9b428ef2c27f92f580b4152a8
IanCardoso <iancardoso@gmail.com>
2025-04-01 12:13:18 -0300

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from '.././scripts/utils/money.js'
+
 export function getProduct (productId) {
   let matchingProduct
 
@@ -9,8 +11,33 @@ export function getProduct (productId) {
 
   return matchingProduct
  }
+
+ class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor (productDetails) {
+    this.id = productDetails.id
+    this.image = productDetails.image
+    this.name = productDetails.name
+    this.rating = productDetails.rating
+    this.priceCents = productDetails.priceCents
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`
+  }
+ }
  
  export const products = [
+  
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -669,4 +696,10 @@ export function getProduct (productId) {
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails)
+});
+
+
+// Em vez de colocar o new product em cada produto o map ele ja coloca no comeco o new Product(productDetails) antes de cada produto onde o (productDetails) é o produto
+
